8029e1ea445f7672a9f70e549e03395314939351
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-15 18:13:47 +0600

    booking api created

--- index.js
+++ index.js
@@ -15,20 +15,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
-        await client.connect();
-        const serviceCollection = client.db('doctors_portal').collection('services');
+  try {
+    await client.connect();
+    const serviceCollection = client.db('doctors_portal').collection('services');
+    const bookingCollection = client.db('doctors_portal').collection('bookings');
 
-        app.get('/service', async (req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-    }
-    finally {
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    }
+    /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking 
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+    */
+
+    app.post('/booking', async (res, req) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(body);
+      res.send(result);
+    })
+
+  }
+  finally {
+
+  }
 
 }
 
